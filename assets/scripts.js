44e9e15f6aabb6fffdf2515e96ae23847fa785a0
Mo7amadMamdou7 <mo7amed.mamdou790@gmail.com>
2025-04-14 23:07:31 +0300

    initial commit

---
+++ assets/scripts.js
@@ -0,0 +1,15 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+  if ($(window).width() <= 991) {
+    $('[data-toggle="tooltip"]').tooltip('disable');
+  }
+  $('select').each(function () {
+    $(this).select2({
+      theme: 'bootstrap4',
+      width: $(this).data('width') ? $(this).data('width') : $(this).hasClass(
+        'w-100') ? '100%' : 'style',
+      placeholder: $(this).data('placeholder'),
+      allowClear: Boolean($(this).data('allow-clear')),
+    });
+  });
+})
